597a31382336881734e1b819ec974075b5613c82
Ryan <franciscogutierrez@Franciscos-MacBook-Pro.local>
2019-03-02 14:56:25 -0500

    setup

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Creating seed data for the burgers_db database --
+use burgers_db
+
+INSERT INTO burgers (burgerName, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burgerName, devoured) VALUES ('Fried Egg Avocado Burger', false);
+INSERT INTO burgers (burgerName, devoured) VALUES ('Mushroom Swiss Burger', false);
